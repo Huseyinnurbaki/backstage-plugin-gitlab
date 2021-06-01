0fff5356b7806de29bb059e8d7fba562b58413d0
huseyin <huseyin.nurbaki@gmail.com>
2021-06-01 11:39:50 +0300

    Add: useProjectIDFromEntity null check

--- src/components/GitlabCard/useProjectIDFromEntity.ts
+++ src/components/GitlabCard/useProjectIDFromEntity.ts
@@ -2,5 +2,9 @@ import { useEntity } from '@backstage/plugin-catalog-react';
 
 export const useProjectIDFromEntity = () => {
   const { entity } = useEntity();
-  return entity.metadata.annotations?.["gitlab/projectID"] ?? '';
+  const annotations = entity?.metadata?.annotations;
+  return annotations?.["gitlab/projectID"] ?? '';
+
+
+  // return entity.metadata.annotations?.["gitlab/projectID"] ?? '';
 };
